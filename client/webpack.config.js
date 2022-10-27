75836495622c75b9967fc5527a3a8c8ab4a7f905
Joon Park <jsp220@gmail.com>
2022-10-27 16:25:30 -0700

    fix: display saved content from indexedDB upon load

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,24 +7,68 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+	return {
+		mode: 'development',
+		entry: {
+			main: './src/js/index.js',
+			install: './src/js/install.js'
+		},
+		output: {
+			filename: '[name].bundle.js',
+			path: path.resolve(__dirname, 'dist'),
+		},
+		plugins: [
+			// Webpack plugin to generate html file and bundles
+			new HtmlWebpackPlugin({
+				template: './index.html',
+				title: 'Just Another Text Editor'
+			}),
 
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+			new InjectManifest({
+				swSrc: './src-sw.js',
+				swDest: 'src-sw.js',
+			}),
+
+			new WebpackPwaManifest({
+				fingerprints: false,
+				inject: true,
+				name: 'Just Another Text Editor',
+				short_name: 'J.A.T.E.',
+				description: 'Takes notes with JavaScript syntax highlighting!',
+				background_color: '#225ca3',
+				theme_color: '#225ca3',
+				start_url: './',
+				publicPath: './',
+				icons: [
+					{
+						src: path.resolve('src/images/logo.png'),
+						sizes: [96, 128, 192, 256, 384, 512],
+						destination: path.join('assets', 'icons'),
+					},
+				],
+			}),
+
+		],
+
+		module: {
+			rules: [
+				{
+					test: /\.css$/i,
+					use: ['style-loader', 'css-loader'],
+				},
+				{
+					test: /\.m?js$/,
+					exclude: /node_modules/,
+					
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env'],
+							plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+						},
+					},
+				},
+			],
+		},
+	};
 };
